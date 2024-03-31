a90802529b062961ca8ba0e5d8245683faaaf18f
Vitor Zuchi <vitorzuchi207gmail.com>
2024-03-31 17:18:33 -0300

    b

---
+++ classmarket.cs
@@ -0,0 +1,16 @@
+using System;
+
+Public class market{
+Public string Product{get;set;}
+Public int Money{get;set;}
+Public string Client{get;set;}
+Public int Value{get;set;}
+Public bool Aproved{get;set;}
+
+Public void Sell()
+{
+}
+Public void Buy()
+{
+}
+}
